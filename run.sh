745004b4a2e290d3c371f94676e02b03ee1369a3
Marcel Muzyczuk <60583279+cicis12@users.noreply.github.com>
2026-04-02 08:50:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Forge requires a configured set of both JVM and program arguments.
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.219/unix_args.txt "$@"
+cd "$(dirname "$0")"
+
+exec java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.219/unix_args.txt "$@" nogui
